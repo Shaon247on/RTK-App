25b802f9eeba2f3578153eab274153b55841fb7e
shaon <aminul007.ais@gmail.com>
2025-08-06 02:12:20 +0600

    first commit

---
+++ app/dashboard/projects/page.tsx
@@ -0,0 +1,10 @@
+import ProjectTable from '@/components/models/projects/ProjectTable'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <ProjectTable/>
+    </div>
+  )
+}
